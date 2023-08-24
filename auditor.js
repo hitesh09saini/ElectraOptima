1ddd490ee211fb9bf853c3a49589a3f251e138dd
hitesh09saini <hitesh9694saini@gmail.com>
2023-08-24 18:01:46 +0530

    home

---
+++ auditor.js
@@ -0,0 +1,26 @@
+var audio = document.getElementById('audio');
+
+document.addEventListener("DOMContentLoaded", function() {
+    // audio.volume = 0.3;
+    audio.play();
+    // console.log(audio);
+});
+function start(){
+
+        audio.pause();
+        document.getElementById('cont1').style.display= 'none';
+        // document.body.style.background = 'red';
+        document.getElementById('cont2').style.display= 'flex';
+}
+
+function comm(){
+    alert("I regret to inform you that it's not presently accessible.");
+}
+
+function indu(){
+    alert("I regret to inform you that it's not presently accessible.");
+}
+
+function home(){
+    location.href = 'Residential.html';
+}
