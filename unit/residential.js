a5b2a0e8a1e374695e13c9d0f504750492edfdf2
hitesh09saini <hitesh9694saini@gmail.com>
2023-08-24 17:22:31 +0530

    home

---
+++ unit/residential.js
@@ -0,0 +1,17 @@
+var les =  document.getElementById("let")
+var txt = 'Welcome to Residential Energy Auditor.';
+var speed = 100;
+var st = "";
+var i =0;
+
+
+function read() {
+    
+    if (i < txt.length) {
+        document.getElementById('wel').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(read, speed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", read);
