f034c3d8fd4c4b38a6188eb30727a72803699cc7
hitesh09saini <hitesh9694saini@gmail.com>
2023-09-25 16:14:47 +0530

    comit

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,47 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
 const port = 3000;
 
-app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
+// Handle POST requests to update the sample.html file
+app.post('/updateSample', (req, res) => {
+  const { inputs } = req.body;
 
-app.get('/', (req, res) => {
-    const data = {
-        message: 'Hello, world!',
-        name: 'Hitesh',
-        class: 25,
-    };
-    res.json(data);
+  // Create or update the sample.html file with the input data
+  const filePath = path.join(__dirname, 'public', 'sample.html');
+  const sampleHtml = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Sample HTML</title>
+    </head>
+    <body>
+      <h1>Sample HTML</h1>
+      <ul>
+        ${inputs.map(input => `<li>${input}</li>`).join('')}
+      </ul>
+    </body>
+    </html>
+  `;
+
+  fs.writeFile(filePath, sampleHtml, (err) => {
+    if (err) {
+      console.error('Error updating sample.html:', err);
+      res.status(500).json({ message: 'Error updating sample.html' });
+    } else {
+      // Respond with the download link
+      res.status(200).json({ downloadUrl: 'sample.html' });
+    }
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
